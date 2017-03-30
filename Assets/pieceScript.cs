c44e0934ca8ae74fb99bd1583ecff17362636389
colewrus <wrampelc@gmail.com>
2017-03-29 23:17:12 -0400

    first commit

---
+++ Assets/pieceScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pieceScript : MonoBehaviour {
+
+	public bool blue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.GetComponent<pieceScript> ().blue != blue) {
+			print ("hit");
+
+		}
+	}
+
+}
